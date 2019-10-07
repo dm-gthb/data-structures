20349dcaf55905f8d984477bc66a213f390eb6c8
Dmitry Mamonov <dmitry.mamonov@corp.mail.ru>
2019-10-07 20:15:21 +0300

    stack & queue

---
+++ queue.js
@@ -0,0 +1,43 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+
+        this.size++;
+    }
+
+    pop() {
+        if(!this.first) {
+            return null;
+        }
+
+        if (this.size === 1) {
+            this.first = null;
+            this.last = null;
+        } else {
+            this.first = this.first.next;
+        }
+        
+        this.size--;
+    }
+}
